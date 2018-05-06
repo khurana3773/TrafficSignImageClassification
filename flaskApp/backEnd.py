ee5ee6d3dcbf17dc6ee7c09af90b8144f11d4e05
surajkhurana <khurana3773@gmail.com>
2018-05-06 02:58:46 -0700

    Working BETA backend

--- flaskApp/backEnd.py
+++ flaskApp/backEnd.py
@@ -2,28 +2,62 @@ from flask import Flask, render_template ,request, jsonify
 import cv2
 import os
 import numpy as np
+import pickle
+from PIL import Image
+import sklearn
+from sklearn.svm import LinearSVC
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+target = os.path.join(APP_ROOT, 'images/')
 
 app = Flask(__name__,template_folder='templates')
 
+extension = "png"
+fileName = "noimage.png"
 
 @app.route('/')
 def main():
     return render_template('hello.html', name='Suraj')
 
+def predict(classifierName="sklearn.MLP"):
+    global fileName, extension
+
+    classifierRoot = APP_ROOT + "\\classifiers\\" +classifierName
+
+    classifier = pickle.load(open(classifierRoot, "rb"))
+
+    print(type(classifier))
+
+    img4 = Image.open( target + "\\" + "processed." + extension )
+
+    image_object = img4.resize((50, 50), Image.ANTIALIAS)
+
+    imageArray = np.array(image_object)
+    listofArrays= []
+    listofArrays.append(imageArray)
+    PREDICT_IMAGES_NP = np.asarray(listofArrays)
+
+    predict = PREDICT_IMAGES_NP.reshape((1, -1))
+    classLabel = classifier.predict(predict)
+
+    return jsonify(
+        imgClass=classLabel[0]
+    )
+
+
 
 
 
 def processImage(file):
+    global fileName,extension
     bgr = [30, 30, 200]
     thresh = 40
     # bright = cv2.imread('C:/Users/Suraj/PycharmProjects/255/Bright.png')
     # dark = cv2.imread(file)
 
-    bright = cv2.imread(file.filename)
+    bright = cv2.imread(target + "\\" + fileName) #TODO : remove hard coding
 
-    bright2 = cv2.imread(file.filename)
+    bright2 = cv2.imread(target + "\\" + fileName)
 
     minBGR = np.array([bgr[0] - thresh, bgr[1] - thresh, bgr[2] - thresh])
     maxBGR = np.array([bgr[0] + thresh, bgr[1] + thresh, bgr[2] + thresh])
@@ -56,32 +90,33 @@ def processImage(file):
 
     boundary = 5
     imgCropped = bright2[boundY - boundary:boundY + maxW + boundary, boundX - boundary:boundX + maxW + boundary]
-    cv2.imwrite("images/processed.jpg", imgCropped)
-
-
+    cv2.imwrite(target + "\\" + "processed."+extension, imgCropped)
 
 
 @app.route('/upload_avatar',methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, 'images/')
+    global fileName, extension
     print(target)
     if not os.path.isdir(target):
         os.mkdir(target)
 
     file = request.files.get('file')
-    destination = "/".join([target,"getClass.jpg"])
+    fileName=file.filename
+    extension= fileName.split('.')[-1]
+
+    destination = "/".join([target,file.filename])
     file.save(destination)
 
+    #TODO: remove hardcoding, currently processing image and storing it at /images as processed.jpg
     processImage(file)
 
-    return jsonify(
-        imgClass = "Class 1"
-    )
+    retObj =  predict("linSvcClassifier.p")
+
+    return retObj
+
+
 
 if __name__ == "__main__":
     app.run()
 
-
-def loadClassifier(classifierName):
-    print("loading classifier" + classifierName)
 
